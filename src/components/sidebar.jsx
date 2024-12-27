99f4b87c59c6de488e0d2a9ce2740ab1cd528d13
Marcio <marcioviniciusfilho760@gmail.com>
2024-12-27 13:38:14 -0300

    Finalizando Sidebar

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,3 +1,5 @@
+import { PencilSimpleLine } from "phosphor-react"
+
 import styles from "./sidebar.module.css"
 
 export function SideBar() {
@@ -9,12 +11,20 @@ export function SideBar() {
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=400&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+
         <strong>João</strong>
         <span>web developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilSimpleLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   )
