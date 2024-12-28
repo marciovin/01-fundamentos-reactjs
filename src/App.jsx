84bdd4058536f501a15265314ffe6f70cdf95f2e
Marcio <marcioviniciusfilho760@gmail.com>
2024-12-28 13:49:59 -0300

    componente:Post

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 import { Header } from "./components/Header"
-import { Post } from "./Post"
+import { Post } from "./components/Post"
 
 import styles from "./App.module.css"
 import "./global.css"
---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from "./Post.module.css"
+
+export function Post(props) {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://plus.unsplash.com/premium_photo-1678197937465-bdbc4ed95815?q=80&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+
+          <div className={styles.authorInfo}>
+            <strong>Fedrerico Forró</strong>
+            <span>back-end developer</span>
+          </div>
+        </div>
+
+        <time datetime="2022-05-11 01:58:41">publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>iubuiuduiwd </p>
+
+        <p>grgrjyjthjbfsdehjbsbhjsd</p>
+
+        <p>
+          {" "}
+          <a href="">bhjbhebewbhkn </a>
+        </p>
+
+        <p>
+          <a href=""> ddddawdddddddddddda </a>
+        </p>
+      </div>
+    </article>
+  )
+}
